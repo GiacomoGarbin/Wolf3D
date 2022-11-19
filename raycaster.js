753a448a7ee087507e22a48e0789431bc7fdf9ef
Giacomo Garbin <giacomogarbinmailbox@gmail.com>
2022-11-19 01:59:27 +0000

    draw grid: walls and empty cells

--- raycaster.js
+++ raycaster.js
@@ -35,10 +35,12 @@ function main() {
     document.onkeyup = shortcuts;
 
     const vsSource = `
+    precision highp float;
     attribute vec4 aVertexPosition;
+    uniform float uPointSize;
     void main(void) {
       gl_Position = aVertexPosition;
-      gl_PointSize = 4.0;
+      gl_PointSize = uPointSize;
     }
   `;
 
@@ -59,6 +61,7 @@ function main() {
         },
         uniformLocations: {
             uFragColor: gl.getUniformLocation(shaderProgram, "uFragColor"),
+            uPointSize: gl.getUniformLocation(shaderProgram, "uPointSize"),
         },
     };
 
@@ -71,6 +74,10 @@ function main() {
     };
 
     prevTime = 0.0
+
+    globals.x = globals.canvas.width / 2;
+    globals.y = globals.canvas.height / 2;
+    globals.angle = -Math.PI / 2;
 
     function render(currTime) {
         const dt = currTime - prevTime;
@@ -139,7 +146,7 @@ function processInput(dt) {
 
     // are we moving diagonally?
     if (n == 2) {
-        // this second if it is not necessary as opposite directions cancel each other out
+        // this second check is not necessary as opposite directions cancel each other out
         if ((globals.keys[KEY_W] != globals.keys[KEY_S]) || (globals.keys[KEY_LEFT] != globals.keys[KEY_RIGHT])) {
             // then modulate step, TODO: cache sqrt(2)/2
             step *= Math.sqrt(2) / 2;
@@ -152,32 +159,40 @@ function processInput(dt) {
     [globals.x, globals.y] = translate([globals.x, globals.y], [-dx, -dy]);
     [globals.x, globals.y] = rotate([globals.x, globals.y], -globals.angle);
 
+    // move forward
     if (globals.keys[KEY_W]) {
         globals.x = globals.x + step;
     }
 
+    // strafe left
     if (globals.keys[KEY_LEFT]) {
         globals.y = globals.y - step;
     }
 
+    // move backward
     if (globals.keys[KEY_S]) {
         globals.x = globals.x - step;
     }
 
+    // strafe right
     if (globals.keys[KEY_RIGHT]) {
         globals.y = globals.y + step;
     }
 
+    // modulate with dt
     const theta = dt * 0.005;
 
+    // rotate left
     if (globals.keys[KEY_A]) {
         globals.angle = globals.angle - theta;
     }
 
+    // rotate right
     if (globals.keys[KEY_D]) {
         globals.angle = globals.angle + theta;
     }
 
+    // clamp angle
     if (globals.angle < 0.0) {
         globals.angle += 2 * Math.PI;
     } else if (globals.angle > 2 * Math.PI) {
@@ -188,9 +203,9 @@ function processInput(dt) {
     [globals.x, globals.y] = rotate([globals.x, globals.y], globals.angle);
     [globals.x, globals.y] = translate([globals.x, globals.y], [dx, dy]);
 
+    // clamp position
     globals.x = Math.max(0, globals.x);
     globals.x = Math.min(globals.x, globals.canvas.height - 1);
-
     globals.y = Math.max(0, globals.y);
     globals.y = Math.min(globals.y, globals.canvas.width - 1);
 }
@@ -210,24 +225,78 @@ function screenSpaceToNDC(vertices) {
 }
 
 function initBuffers(gl) {
-    const playerBuffer = gl.createBuffer();
+
+    const grid = [
+        1, 1, 1, 1, 1, 1, 1, 1,
+        1, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 0, 0, 1, 1, 0, 1,
+        1, 0, 1, 0, 0, 1, 0, 1,
+        1, 0, 1, 0, 0, 1, 0, 1,
+        1, 0, 1, 1, 0, 0, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 1,
+        1, 1, 1, 1, 1, 1, 1, 1,
+    ];
+
+    const rows = 8;
+    const cols = 8;
+
+    console.assert(grid.length == rows * cols);
+
+    const size = 64;
+    const half = size / 2;
+
+    let walls = [];
+    let cells = [];
+
+    for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+            const i = row * cols + col;
+
+            const x = col * size + half;
+            const y = row * size + half;
+
+            switch (grid[i]) {
+                case 0:
+                    cells.push(x, y);
+                    break;
+                case 1:
+                    walls.push(x, y);
+                    break;
+            }
+        }
+    }
+
+    // ========== walls ==========
+
+    const wallsBuffer = gl.createBuffer();
 
     // select the buffer to apply buffer operations to from here out
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, playerBuffer);
+    gl.bindBuffer(gl.ARRAY_BUFFER, wallsBuffer);
 
     // position in NDC
-
-    const positions = screenSpaceToNDC([globals.x, globals.y])
+    walls = screenSpaceToNDC(walls)
 
     // fill the current buffer
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(walls), gl.STATIC_DRAW);
 
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+    // ========== cells ==========
+
+    const cellsBuffer = gl.createBuffer();
+
+    // select the buffer to apply buffer operations to from here out
+    gl.bindBuffer(gl.ARRAY_BUFFER, cellsBuffer);
+
+    // position in NDC
+    cells = screenSpaceToNDC(cells)
+
+    // fill the current buffer
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cells), gl.STATIC_DRAW);
 
     return {
-        grid: null,
-        player: null,
-        rays: null,
+        walls: { buffer: wallsBuffer, count: walls.length / 2, },
+        cells: { buffer: cellsBuffer, count: cells.length / 2, },
+        player: { buffer: null, count: 0, },
+        rays: { buffer: null, count: 0, },
     };
 }
 
@@ -235,7 +304,7 @@ function updateBuffers(gl, buffers) {
 
     // ========== player ==========
 
-    gl.deleteBuffer(buffers.player);
+    gl.deleteBuffer(buffers.player.buffer);
     const playerBuffer = gl.createBuffer();
 
     // select the buffer to apply buffer operations to from here out
@@ -249,12 +318,12 @@ function updateBuffers(gl, buffers) {
 
     // ========== rays ==========
 
-    gl.deleteBuffer(buffers.rays);
+    gl.deleteBuffer(buffers.rays.buffer);
     const raysBuffer = gl.createBuffer();
 
     gl.bindBuffer(gl.ARRAY_BUFFER, raysBuffer);
 
-    const length = 100.0;
+    const length = 20.0;
     const rays = screenSpaceToNDC([
         globals.x,
         globals.y,
@@ -264,9 +333,10 @@ function updateBuffers(gl, buffers) {
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(rays), gl.STATIC_DRAW);
 
     return {
-        grid: buffers.grid,
-        player: playerBuffer,
-        rays: raysBuffer,
+        walls: buffers.walls,
+        cells: buffers.cells,
+        player: { buffer: playerBuffer, count: player.length / 2, },
+        rays: { buffer: raysBuffer, count: rays.length / 2, },
     };
 }
 
@@ -319,13 +389,78 @@ function loadShader(gl, type, source) {
 }
 
 function drawScene(gl, programInfo, buffers) {
-    gl.clearColor(0.5, 0.5, 0.5, 1.0);
+    gl.clearColor(0.2, 0.2, 0.2, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     gl.useProgram(programInfo.program);
 
-    // draw grid
+    // draw walls
 
+    {
+        const numComponents = 2;
+        const type = gl.FLOAT;
+        const normalize = false;
+        const stride = 0;
+        const offset = 0;
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.walls.buffer);
+        gl.vertexAttribPointer(
+            programInfo.attribLocations.vertexPosition,
+            numComponents,
+            type,
+            normalize,
+            stride,
+            offset
+        );
+        gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
+    }
+
+    {
+        const color = [0.5, 0.5, 0.5, 1.0];
+        gl.uniform4fv(programInfo.uniformLocations.uFragColor, new Float32Array(color));
+
+        gl.uniform1f(programInfo.uniformLocations.uPointSize, 63.0);
+    }
+
+    {
+        const mode = gl.POINTS;
+        const first = 0;
+        const count = buffers.walls.count;
+        gl.drawArrays(mode, first, count);
+    }
+
+    // draw cells
+
+    {
+        const numComponents = 2;
+        const type = gl.FLOAT;
+        const normalize = false;
+        const stride = 0;
+        const offset = 0;
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.cells.buffer);
+        gl.vertexAttribPointer(
+            programInfo.attribLocations.vertexPosition,
+            numComponents,
+            type,
+            normalize,
+            stride,
+            offset
+        );
+        gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
+    }
+
+    {
+        const color = [0.3, 0.3, 0.3, 1.0];
+        gl.uniform4fv(programInfo.uniformLocations.uFragColor, new Float32Array(color));
+
+        gl.uniform1f(programInfo.uniformLocations.uPointSize, 63.0);
+    }
+
+    {
+        const mode = gl.POINTS;
+        const first = 0;
+        const count = buffers.cells.count;
+        gl.drawArrays(mode, first, count);
+    }
 
     // draw rays
 
@@ -335,7 +470,7 @@ function drawScene(gl, programInfo, buffers) {
         const normalize = false;
         const stride = 0;
         const offset = 0;
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.rays);
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.rays.buffer);
         gl.vertexAttribPointer(
             programInfo.attribLocations.vertexPosition,
             numComponents,
@@ -350,12 +485,14 @@ function drawScene(gl, programInfo, buffers) {
     {
         const color = [1.0, 0.0, 1.0, 1.0];
         gl.uniform4fv(programInfo.uniformLocations.uFragColor, new Float32Array(color));
+
+        // gl.uniform1f(programInfo.uniformLocations.uPointSize, 4.0);
     }
 
     {
         const mode = gl.LINES;
         const first = 0;
-        const count = 2;
+        const count = buffers.rays.count;
         gl.drawArrays(mode, first, count);
     }
 
@@ -367,7 +504,7 @@ function drawScene(gl, programInfo, buffers) {
         const normalize = false;
         const stride = 0;
         const offset = 0;
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.player);
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.player.buffer);
         gl.vertexAttribPointer(
             programInfo.attribLocations.vertexPosition,
             numComponents,
@@ -379,13 +516,17 @@ function drawScene(gl, programInfo, buffers) {
         gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
     }
 
-    const color = [0.0, 1.0, 0.0, 1.0];
-    gl.uniform4fv(programInfo.uniformLocations.uFragColor, new Float32Array(color));
+    {
+        const color = [0.0, 1.0, 0.0, 1.0];
+        gl.uniform4fv(programInfo.uniformLocations.uFragColor, new Float32Array(color));
+
+        gl.uniform1f(programInfo.uniformLocations.uPointSize, 4.0);
+    }
 
     {
         const mode = gl.POINTS;
         const first = 0;
-        const count = 1;
+        const count = buffers.player.count;
         gl.drawArrays(mode, first, count);
     }
 }
